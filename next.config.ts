23018b68912dacf22bb60d7ab9ec7320e3ef4c00
DMG PRODUÇOES <dmgproductionsoficial@gmail.com>
2026-01-16 22:17:50 +0000

    GET /area-do-cliente 200 in 1373ms  GET /area-do-cliente 200 in 261ms  G

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
+import withPWA from '@ducanh2912/next-pwa';
 
-const withPWA = require('@ducanh2912/next-pwa')({
+const pwaConfig = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
@@ -39,4 +40,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+export default pwaConfig(nextConfig);
