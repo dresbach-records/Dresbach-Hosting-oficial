4e60609c1ea0c0672e41427c0fd96a9fab838b3e
DMG PRODUÇOES <dmgproductionsoficial@gmail.com>
2026-01-16 21:42:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3513285477-702dc",
+  "appId": "1:51763356327:web:cab97bedaac3019257f182",
+  "apiKey": "AIzaSyBFiGBS0GTHqveQ7UGL6LH8VFwyd4Zj9Lk",
+  "authDomain": "studio-3513285477-702dc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "51763356327"
+};
