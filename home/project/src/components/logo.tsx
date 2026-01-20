01b74c7cf6012a33b490595c80e99e89ac96cc57
DMG PRODUÇOES <dmgproductionsoficial@gmail.com>
2026-01-20 11:43:11 +0000

    logo quebrada nao aprece

--- home/project/src/components/logo.tsx
+++ home/project/src/components/logo.tsx
@@ -4,7 +4,7 @@ import { cn } from '@/lib/utils';
 export function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="/logo nova.png"
+      src="/logo%20nova.png"
       alt="Dresbach Hosting Logo"
       width={180}
       height={45}
